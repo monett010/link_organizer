5fe563e674d38b5ce739dd28116b0d36320bae5c
monett010 <acts2018+gh@gmail.com>
2025-05-06 23:03:12 -0400

    Wrote Connection class/logic for SQL inserts and selects. Wrote SQLStatements class and methods for getting and inserting.

---
+++ Controller.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+class Connection ():
+
+    def __init__(self):
+        self.db = "Bookmarks.db"
+
+    def sql_get (self, sql_statement):
+        connection_ = sqlite3.connect(self.db)
+        cursor = connection_.cursor()
+        rows_ = cursor.execute(sql_statement)
+        rows = rows_.fetchall()
+        cursor.close()
+        connection_.close()
+        return rows
+
+    def sql_insert (self, sql_statement, params):
+        connection_ = sqlite3.connect(self.db)
+        cursor = connection_.cursor()
+        cursor.execute(sql_statement, params)
+        connection_.commit()
+        cursor.close()
+        connection_.close()
+
+
+class SQLStatements (Connection) :
+
+    # GET
+    def getBookmarks (self, opt:str) -> dict:
+        match opt:
+            case "all":
+                sql_statement = "SELECT * FROM Bookmarks;"
+            case "u":
+                sql_statement = "SELECT * FROM Bookmarks WHERE archived='N';"
+            case "a":
+                sql_statement = "SELECT * FROM Bookmarks WHERE archived='Y';"
+
+        bookmarks = Connection.sql_get(self, sql_statement)
+        return bookmarks
+
+    def getTags (self) -> dict:
+        sql_statement = "SELECT * FROM Tags;"
+        tags = Connection.sql_get(self, sql_statement)
+        return tags
+    
+    # INSERT
+    def addBookmark (self, params_:tuple) -> None:
+        sql_statement = "INSERT INTO Bookmarks (bookmark_title,bookmark_url,date_added) VALUES (?,?,?);"
+        Connection.sql_insert(self, sql_statement, params_)
+
+    def addTag (self, tag_name:str) -> None:
+        sql_statement = "INSERT INTO Tags (tag_name) VALUES (?);"
+        Connection.sql_insert(self, sql_statement, (tag_name,))
+
+    def addTagToBookmark (self, bookmark_id, tag_id) -> None:
+        sql_statement = "INSERT INTO Bookmark_tags (bookmark_id, tag_id) VALUES (?,?);"
+        Connection.sql_insert(self, sql_statement, (bookmark_id, tag_id,))
---
+++ testing.py
@@ -0,0 +1,24 @@
+from Controller import Connection, SQLStatements
+
+s = SQLStatements()
+
+# TESTING GET BOOKMARKS
+# print (s.getBookmarks("a"))
+
+# TESTING GET TAGS
+# print (s.getTags())
+
+#TESTING ADDING BOOKMARK
+
+# bookmark_title = "How To Define A Function With Optional Arguments?"
+# bookmark_url = "https://stackoverflow.com/questions/9539921/how-do-i-define-a-function-with-optional-arguments"
+# date_added = "2025-05-06"
+
+# s.addBookmark ((bookmark_title, bookmark_url, date_added,))
+
+
+# TESTING ADDING TAG
+# s.addTag ("documentation still rules")
+
+# TESTING ADDING TAG TO BOOKMARK
+# s.addTagToBookmark ("6","3")

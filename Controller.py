8291843b8c872de6545d88d8242eb9c29150a923
monett010 <acts2018+gh@gmail.com>
2025-05-07 12:50:56 -0400

    Deleted Connection.sql_delete(). Renamed Connection.sql_get to sql_fetch(). Replaced calls to old names with new ones.

--- Controller.py
+++ Controller.py
@@ -5,7 +5,7 @@ class Connection ():
     def __init__(self):
         self.db = "Bookmarks.db"
 
-    def sql_get (self, sql_statement):
+    def sql_fetch (self, sql_statement):
         connection_ = sqlite3.connect(self.db)
         cursor = connection_.cursor()
         rows_ = cursor.execute(sql_statement)
@@ -14,15 +14,7 @@ class Connection ():
         connection_.close()
         return rows
 
-    def sql_insert (self, sql_statement, params):
-        connection_ = sqlite3.connect(self.db)
-        cursor = connection_.cursor()
-        cursor.execute(sql_statement, params)
-        connection_.commit()
-        cursor.close()
-        connection_.close()
-
-    def sql_delete (self, sql_statement, params):
+    def sql_commit (self, sql_statement, params):
         connection_ = sqlite3.connect(self.db)
         cursor = connection_.cursor()
         cursor.execute(sql_statement, params)
@@ -44,7 +36,7 @@ class SQLStatements (Connection) :
             case "a":
                 sql_statement = "SELECT * FROM Bookmarks WHERE archived='Y';"
 
-        bookmarks_ = Connection.sql_get(self, sql_statement)
+        bookmarks_ = Connection.sql_fetch(self, sql_statement)
         bookmarks = dict()
 
         for b in bookmarks_:
@@ -56,7 +48,7 @@ class SQLStatements (Connection) :
 
     def getTags (self) -> dict:
         sql_statement = "SELECT * FROM Tags;"
-        tags_ = Connection.sql_get(self, sql_statement)
+        tags_ = Connection.sql_fetch(self, sql_statement)
 
         tags = dict()
 
@@ -73,28 +65,28 @@ class SQLStatements (Connection) :
     def addBookmark (self, params_:dict) -> None:
         sql_statement = "INSERT INTO Bookmarks (bookmark_title,bookmark_url,date_added) VALUES (?,?,?);"
         params = (params_['bookmark_title'], params_['bookmark_url'], params_['date_added'],)
-        Connection.sql_insert(self, sql_statement, params)
+        Connection.sql_commit(self, sql_statement, params)
 
     def addTag (self, tag_name:str) -> None:
         sql_statement = "INSERT INTO Tags (tag_name) VALUES (?);"
-        Connection.sql_insert(self, sql_statement, (tag_name,))
+        Connection.sql_commit(self, sql_statement, (tag_name,))
 
     def addTagToBookmark (self, params_:dict) -> None:
         sql_statement = "INSERT INTO Bookmark_tags (bookmark_id, tag_id) VALUES (?,?);"
         params = (params_['bookmark_id'], params_['tag_id'],)
-        Connection.sql_insert(self, sql_statement, params)
+        Connection.sql_commit(self, sql_statement, params)
 
     # DELETE
     def removeBookmark (self, bookmark_id:int) -> None:
         sql_statement = "DELETE FROM Bookmarks WHERE bookmark_id = ?;"
-        Connection.sql_delete(self, sql_statement, (bookmark_id,))
+        Connection.sql_commit(self, sql_statement, (bookmark_id,))
 
     def removeTag (self, tag_id:int) -> None:
         sql_statement = "DELETE FROM Tags WHERE tag_id = ?;"
-        Connection.sql_delete(self, sql_statement, (tag_id,))
+        Connection.sql_commit(self, sql_statement, (tag_id,))
     
 
     def removeTagFromBookmark (self, params_) -> None:
         sql_statement = "DELETE FROM Bookmark_tags WHERE tag_id = ? AND bookmark_id = ?;"
         params = (params_['tag_id'],params_['bookmark_id'],)
-        Connection.sql_delete(self,sql_statement, params)
+        Connection.sql_commit(self,sql_statement, params)

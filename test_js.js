a8ace1575408c4389e9c2ff6a32eb7e42a1be626
monett010 <acts2018+gh@gmail.com>
2025-05-29 18:47:52 -0400

    Added tags to bookmarks cards.

--- test_js.js
+++ test_js.js
@@ -11,6 +11,12 @@ async function getBookmarks(){
     return json;
 }
 
+async function getBookmarkTags(bookmark_id){
+    const response = await fetch("http://127.0.0.1:5000/bookmarks/" + bookmark_id + "/tags/")
+    const json = await response.json();
+    return json;
+}
+
 async function logBookmark(){
     const bookmark = await getBookmark();
     console.log(bookmark);
@@ -25,13 +31,25 @@ async function writeBookmark(){
     bookmark_list_section.innerHTML = bookmark_html;   
 }
 
+async function printTags(bookmark_id) {
+    const bookmark_tags = await getBookmarkTags (bookmark_id);
+    let tags = "";
+    for (t in bookmark_tags){
+        tags += bookmark_tags[t]['tag_name'] + " | ";
+    }
+    return tags.substring(0, tags.length - 3);
+}
+
 // writes unarchived bookmarks to home page
 async function writeBookmarks() {
     const bookmarks = await getBookmarks();
     const node = document.getElementById("bookmark_list");
+    // const bookmark_tags = await printTags();
 
     for (b in bookmarks) {
-        const bookmark_html = "<div class='bookmark'> <ul><li><a href='bookmark.html'>" + bookmarks[b]['bookmark_title'] + "</a></li><li><a href='" + bookmarks[b]['bookmark_url'] + "'>" + bookmarks[b]['bookmark_url'] + "</a></li> <li>tag | tag | tag</li></ul></div>";
+        const bookmark_tags = await printTags(b);
+        const bookmark_html = "<div class='bookmark' id='" + b + "'> <ul><li><a href='bookmark.html'>" + bookmarks[b]['bookmark_title'] + "</a></li><li><a href='" + bookmarks[b]['bookmark_url'] + "'>" + bookmarks[b]['bookmark_url'] + "</a></li><li>" + bookmark_tags + "</li></ul></div>";
         node.innerHTML += bookmark_html;
     }
 }
+
